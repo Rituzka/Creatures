10b8a3b6c0c73c672913e92e792d4ea9c75a3c5c
Rita Casiello <rituzka@gmail.com>
2020-07-22 17:57:11 +0200

    starting project, Room and database implemented

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureGenerator.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.android.creaturemon.model
+
+class CreatureGenerator {
+    fun generateCreature(attributes: CreatureAttributes, name: String = "",
+    drawable: Int = 0) : Creature {
+        val hitpoints = 5 * attributes.intelligence +
+                3 * attributes.strength +
+                4 * attributes.endurance
+        return Creature(attributes, hitpoints, name, drawable)
+    }
+
+}
